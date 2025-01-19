aef7708e6fcd1f4a9b8010e50c92ef400be6d06b
fktona <adetona.fk@gmail.com>
2025-01-19 23:47:53 +0100

    done

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,75 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className=" text-white relative isolate w-full h-dvh">
+      <Image
+        src="/bg.png"
+        width={1920}
+        height={1080}
+        objectFit="cover"
+        alt="bg"
+        className="w-full  h-full absolute object-cover -z-10"
+      />
+      <Image
+        src="/noise.png"
+        width={1920}
+        height={1080}
+        objectFit="cover"
+        alt="bg"
+        className="w-full h-full absolute object-cover -z-10"
+      />
+      <Navbar />
+      <div className="text-[15px] w-full lg:w-auto flex flex-col items-center gap-3 absolute bottom-6 px-5 ">
+        <div className=" px-3  lg:hidden flex gap-1 items-center py-2 ">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#"
+            className="  w-16 h-16 flex items-center rounded-tl-2xl rounded-bl-2xl justify-center lg:rounded-tl-lg lg:rounded-bl-lg bg-white/10"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            <Image src="/x.png" width={30} height={30} alt="x" />
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#"
+            className="  w-16 h-16 flex items-center justify-center  bg-white/10"
           >
-            Read our docs
+            <Image src="/truth.png" width={30} height={30} alt="truth" />
+          </a>
+          <a
+            href="#"
+            className=" w-16 h-16 flex  rounded-tr-2xl rounded-br-2xl items-center justify-center lg:rounded-tr-lg lg:rounded-br-lg bg-white/10"
+          >
+            <Image src="/pump.png" width={30} height={30} alt="pump" />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <span className="">Built with TruthIntel & Eliza 💙</span>
+      </div>
     </div>
   );
 }
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-[80px] font-inter  flex justify-between items-center px-5">
+      <h2 className="text-[40px] uppercase font-digital">TrustEliza</h2>
+      <div className=" px-3 lg:flex gap-1 hidden items-center py-2 ">
+        <a
+          href="#"
+          className="  w-12 h-12 hover:bg-black/40 active:scale-95 flex items-center justify-center lg:rounded-tl-lg  lg:rounded-bl-lg bg-white/10"
+        >
+          <Image src="/x.png" width={24} height={24} alt="x" />
+        </a>
+        <a
+          href="#"
+          className="  w-12 h-12 flex hover:bg-black/40 active:scale-95 items-center justify-center  bg-white/10"
+        >
+          <Image src="/truth.png" width={24} height={20} alt="truth" />
+        </a>
+        <a
+          href="#"
+          className=" w-12 h-12 flex hover:bg-black/40 active:scale-95 items-center justify-center lg:rounded-tr-lg  lg:rounded-br-lg  bg-white/10"
+        >
+          <Image src="/pump.png" width={24} height={24} alt="pump" />
+        </a>
+      </div>
+    </nav>
+  );
+};
